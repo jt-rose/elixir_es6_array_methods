9c7072501854ba1c04b1fc16ed680a3de0cbdd36
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-07-29 16:57:09 -0400

    create examples of es6 methods in elixir

--- lib/elixir_es6_array_methods.ex
+++ lib/elixir_es6_array_methods.ex
@@ -3,40 +3,59 @@ defmodule ArrayMethods do
   Examples of how to implement EcmaScript 6 array methods in Elixir
   """
 
-  @doc """
-  Hello world.
+  def is_even(num) do
+    rem(num, 2) == 0
+  end
 
-  ## Examples
-
-      iex> ElixirEs6ArrayMethods.hello()
-      :world
-
-  """
   def map(array) do
-    :example
+    # double numbers
+    Enum.map(array, fn x -> x * 2 end)
   end
 
   def filter(array) do
-    :example
+    # keep only evens
+    array |> Enum.filter(&is_even/1)
+    # Enum.filter(array, fn x -> x % 2 == 0 end)
   end
 
   def find(array) do
-    :example
+    # find even
+    Enum.find(array, fn x -> rem(x, 2) == 0 end)
   end
 
   def findIndex(array) do
-    :example
+    # find first even number
+    Enum.find_index(array, fn x -> rem(x, 2) == 0 end)
   end
 
   def some(array) do
-    :example
+    # find if any of the numbers are even
+    Enum.any?(array, fn x -> rem(x, 2) == 0 end)
   end
 
   def every(array) do
-    :example
+    # find if all numbers are even
+    Enum.all?(array, fn x -> rem(x, 2) == 0 end)
   end
 
   def reduce(array) do
-    :example
+    # get sum of all numbers
+    Enum.sum(array)
   end
 end
+
+res = ArrayMethods.map([1,2,3,4,5,6])
+res2 = ArrayMethods.filter([1,2,3,4,5,6])
+res3 = ArrayMethods.find([1,2,3,4,5,6])
+res4 = ArrayMethods.findIndex([1,2,3,4,5,6])
+res5 = ArrayMethods.some([1,2,3,4,5,6])
+res6 = ArrayMethods.every([1,2,3,4,5,6])
+res7 = ArrayMethods.reduce([1,2,3,4,5,6])
+
+IO.puts "Map: #{inspect(res)}"
+IO.puts "Filter: #{inspect(res2)}"
+IO.puts "Find: #{inspect(res3)}"
+IO.puts "FindIndex: #{inspect(res4)}"
+IO.puts "Some: #{inspect(res5)}"
+IO.puts "Every: #{inspect(res6)}"
+IO.puts "Reduce: #{inspect(res7)}"
